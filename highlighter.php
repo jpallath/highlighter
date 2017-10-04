f738a2b73b68304506cda68adbf96c21645fc703
Jerry Pallath <pallathj@gmail.com>
2017-10-03 21:37:16 -0400

    implemented highlights

--- highlighter.php
+++ highlighter.php
@@ -6,3 +6,17 @@
 * Version: 1.0
 * Author: Jerry Pallath
 */
+
+function highlighter($content){
+    // first term:
+    $content = str_replace('is','<span class="highlighted" style="background:yellow;">is</span>', $content);
+    // second term
+    $content = str_replace('WordPress', '<span class="highlighted" style="background:pink;">WordPress</span>', $content);
+    // finished
+    return $content;
+}
+
+add_filter('the_content','highlighter');
+
+?>
+
